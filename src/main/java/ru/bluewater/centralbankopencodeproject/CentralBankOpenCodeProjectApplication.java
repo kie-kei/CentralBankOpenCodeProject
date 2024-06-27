f6fb6b049ad2bc5f0851cbc7a848bf66a208c69e
Maksim Pozdeev <pozdeev_max@mail.ru>
2024-06-28 01:25:45 +0400

    init

---
+++ src/main/java/ru/bluewater/centralbankopencodeproject/CentralBankOpenCodeProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.bluewater.centralbankopencodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBankOpenCodeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralBankOpenCodeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bluewater/centralbankopencodeproject/CentralBankOpenCodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bluewater.centralbankopencodeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralBankOpenCodeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
