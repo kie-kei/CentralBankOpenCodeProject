02f0528c78e1b5795172c48663484184ce032d82
Maksim Pozdeev <pozdeev_max@mail.ru>
2024-07-02 17:54:01 +0400

    проект разбит на 2 модуля api и src
    добавлена Swagger документация
    
    добавлены некоторые ошибки для FileControllerImpl

---
+++ central-bank-rest-src/src/main/java/ru/bluewater/centralbankrestsrc/CentralBankRestSrcApplication.java
@@ -0,0 +1,13 @@
+package ru.bluewater.centralbankrestsrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBankRestSrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralBankRestSrcApplication.class, args);
+    }
+
+}
---
+++ central-bank-rest-src/src/test/java/ru/bluewater/centralbankrestsrc/CentralBankRestSrcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bluewater.centralbankrestsrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralBankRestSrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
