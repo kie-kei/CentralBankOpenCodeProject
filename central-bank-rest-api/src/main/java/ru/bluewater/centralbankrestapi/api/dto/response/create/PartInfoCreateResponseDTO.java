ff433a7a4768ef6be62434e67a465d57e6e2bbab
sklnrr <artemiyilin765@gmail.com>
2024-07-10 03:06:00 +0400

    fixed part info

--- central-bank-rest-api/src/main/java/ru/bluewater/centralbankrestapi/api/dto/response/create/PartInfoCreateResponseDTO.java
+++ central-bank-rest-api/src/main/java/ru/bluewater/centralbankrestapi/api/dto/response/create/PartInfoCreateResponseDTO.java
@@ -1,9 +1,13 @@
 package ru.bluewater.centralbankrestapi.api.dto.response.create;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.UUID;
-
+@Setter
+@Getter
 public class PartInfoCreateResponseDTO {
     private UUID uuid;
     private Integer partNo;
